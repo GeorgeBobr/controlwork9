976a2447137c18a6c15885533731c7d559a67530
Georg <georgestoneix@gmail.com>
2024-08-31 18:31:23 +0600

    Обновил представления

--- controlwork/webapp/views/album_views.py
+++ controlwork/webapp/views/album_views.py
@@ -7,34 +7,35 @@ class AlbumDetailView(DetailView):
     model = Album
     template_name = 'album/album_detail.html'
 
-
 class AlbumCreateView(LoginRequiredMixin, CreateView):
     model = Album
-    fields = ['image', 'caption', 'album', 'is_public']
+    fields = ['title', 'description', 'is_public']
     template_name = 'album/album_create.html'
-    success_url = reverse_lazy('album_list')
+    success_url = reverse_lazy('webapp:photo_list')
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-
 class AlbumUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Album
-    fields = ['image', 'caption', 'album', 'is_public']
+    fields = ['title', 'description', 'is_public']
     template_name = 'album/album_update.html'
-    success_url = reverse_lazy('album_list')
 
     def test_func(self):
         album = self.get_object()
         return self.request.user == album.author
 
-
+    def get_success_url(self):
+        return reverse_lazy('webapp:album_detail', kwargs={'pk': self.object.pk})
 class AlbumDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Album
     template_name = 'album/album_delete.html'
-    success_url = reverse_lazy('album_list')
 
     def test_func(self):
         album = self.get_object()
         return self.request.user == album.author
+
+    def get_success_url(self):
+        user = self.request.user
+        return reverse_lazy('accounts:profile', kwargs={'pk': user.pk})
